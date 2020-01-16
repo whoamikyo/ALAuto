9e258ee6ec34ea49a13baaa78ea79d5637e48e76
kyO The NinjA <erikferreira@outlook.com>
2020-01-16 11:46:04 -0300

    13-1 swipe tweak

--- modules/combat.py
+++ modules/combat.py
@@ -400,6 +400,8 @@ class CombatModule(object):
                 Utils.swipe(1250, 530, 1300, 540, 100)
             elif cmap == '11-3':
                 Utils.swipe(1020, 570, 1300, 540, 100)
+            elif cmap == '13-1':
+                Utils.swipe(1020, 500, 1300, 540, 100)
             else:
                 Utils.swipe(960, 540, 1300, 540, 100)
 
